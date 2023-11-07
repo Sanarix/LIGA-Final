afc7fd312af1f7a09977c93ab7c5c16f7eaf2eae
Sanarix <KorsakovLA@mail.ru>
2023-11-07 11:38:13 +0300

    добавил middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+// import logger from 'redux-logger';
 import tasksListReducer from './slices/tasksList/tasksList.slice';
 
 export const store = configureStore({
   reducer: {
     tasks: tasksListReducer,
   },
+  // middleware: [logger], ругается на logger
 });
 
 export type RootState = ReturnType<typeof store.getState>;
