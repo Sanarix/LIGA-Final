12cb7c7a0cfc7a2588a18ed3e747184f492b25a5
Sanarix <KorsakovLA@mail.ru>
2023-11-09 18:03:05 +0300

    добавил лоадер при загрузке тасков

--- src/app/taskList/list/List.tsx
+++ src/app/taskList/list/List.tsx
@@ -1,4 +1,3 @@
-import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 import type { ListProps } from './List.types';
@@ -7,6 +6,7 @@ import { Task } from 'src/app/taskList/task/Task';
 import { Checkbox } from 'components/Checkbox';
 import { deleteTask } from 'src/slices/tasksList/tasksList.slice';
 import { useTasksSlice } from 'src/slices/tasksList/tasks.hooks';
+import { Loader } from 'components/Loader';
 
 export function List({ tasksArr }: ListProps) {
   const { isLoading, tasks, dispatch, fetchTasks } = useTasksSlice();
@@ -18,31 +18,33 @@ export function List({ tasksArr }: ListProps) {
   return (
     <div className="tasks-wrapper">
       <button onClick={() => handler()}>Получить таски</button>
-      {tasksArr.map((task, i) => {
-        return (
-          <Task key={i}>
-            <div className={styles.checkbox}>
-              <Checkbox label="" containerClassName={styles['checkbox-container']} />
-            </div>
-            <div className={styles.task}>
-              <h2>{task.name}</h2>
-              <p>{task.info}</p>
-            </div>
-            <div className={styles.buttons}>
-              <Link to={`/TaskForm/${task.id}`} className={styles.button}>
-                Edit
-              </Link>
-              <button
-                className={styles.deleteButton}
-                onClick={() => {
-                  dispatch(deleteTask(task.id));
-                }}>
-                Delete
-              </button>
-            </div>
-          </Task>
-        );
-      })}
+      <Loader isLoading={isLoading}>
+        {tasksArr.map((task, i) => {
+          return (
+            <Task key={i}>
+              <div className={styles.checkbox}>
+                <Checkbox label="" containerClassName={styles['checkbox-container']} />
+              </div>
+              <div className={styles.task}>
+                <h2>{task.name}</h2>
+                <p>{task.info}</p>
+              </div>
+              <div className={styles.buttons}>
+                <Link to={`/TaskForm/${task.id}`} className={styles.button}>
+                  Edit
+                </Link>
+                <button
+                  className={styles.deleteButton}
+                  onClick={() => {
+                    dispatch(deleteTask(task.id));
+                  }}>
+                  Delete
+                </button>
+              </div>
+            </Task>
+          );
+        })}
+      </Loader>
     </div>
   );
 }
